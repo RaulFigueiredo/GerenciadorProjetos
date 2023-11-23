dd8891ce79601dfa4aa5aaa2e4ba56a4dd30f994
Breno-Azevedo <eborn.azevedo@gmail.com>
2023-11-23 01:16:57 -0300

    refactor: logic

--- src/logic/__init__.py
+++ src/logic/__init__.py
@@ -20,10 +20,6 @@ from src.logic.calendar.date_utilities import DateUtilities
 from src.logic.calendar.calendar_display import CalendarDisplay
 from src.logic.calendar.month_view import MonthView
 from src.logic.calendar.month_year_navigation import MonthYearNavigation
-<<<<<<< HEAD
 from src.logic.calendar.task_details import TaskDetails
 
 from .history.task_history import HistorySingleton
-=======
-from src.logic.calendar.task_details import TaskDetails
->>>>>>> main
