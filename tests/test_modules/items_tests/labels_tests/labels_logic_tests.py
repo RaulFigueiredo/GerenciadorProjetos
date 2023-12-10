bbbb40106605b85cf3f8712626202ce370f10147
RaulFigueiredo <raullomonte13@gmail.com>
2023-12-10 19:52:11 -0300

    update: test suite label with database

--- tests/test_modules/items_tests/labels_tests/labels_logic_tests.py
+++ tests/test_modules/items_tests/labels_tests/labels_logic_tests.py
@@ -5,95 +5,105 @@ along with interactions with a mocked user object to simulate system operations.
 
 
 import unittest
+from src import Label, User, ItemDontHaveThisAttribute, NonChangeableProperty
+from sqlalchemy.orm import sessionmaker
+from src.logic.orms.orm import UserORM, Base, LabelORM
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import date
 from unittest.mock import Mock
-from src.logic.items.label import Label
-from src.gui.labels.label_create import AddLabelDialog
-from src.gui.labels.label_edit import EditLabelDialog
-from unittest.mock import patch
-import tkinter as tk
-
-from src.logic.execeptions.exceptions_items import  ItemDontHaveThisAttribute,\
-                                                    NonChangeableProperty
-
 class TestLabel(unittest.TestCase):
-    """
-    The TestLabel class contains unit tests for the Label class.
-    """
+    @classmethod
+    def setUpClass(cls):
+        cls.engine = create_engine('sqlite:///:memory:')
+        Base.metadata.create_all(cls.engine)
+        cls.SessionLocal = sessionmaker(bind=cls.engine)
 
-    def setUp(self) -> None:
-        """
-        Sets up the test environment before each test method is called.
-        Initializes a mocked user and a Label object for testing purposes.
-        """
-        self.mock_user: Mock = Mock()
-        self.mock_user.add_label = Mock()
-        self.mock_user.remove_label = Mock()
-        self.mock_user.update_label = Mock()
-        self.label: Label = Label(self.mock_user, "Test Label", "blue")
+        cls.session = TestLabel.SessionLocal()
+
+        db_test_user = UserORM(name='Test User', email='test@example.com', password='teste')
+        cls.session.add(db_test_user)
+        cls.session.commit()
+
+        cls.test_user = User(name=db_test_user.name, id_user=db_test_user.id_user, session=cls.session)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.session.close()
+
+    def setUp(self):
+        self.session = TestLabel.SessionLocal()
+        self.create_test_items()
+
+    def create_test_items(self):
+        db_test_label = LabelORM(name='Test Label', id_user=self.test_user.id_user, color='azul') 
+        self.session.add(db_test_label)
+        self.session.commit()
+
+        self.test_label = Label(name=db_test_label.name,
+                                user=self.test_user,
+                                id_label=db_test_label.id_label,
+                                color=db_test_label.color,
+                                session = self.session)
+
+    def tearDown(self):
+        self.session.close()
 
     def test_init(self) -> None:
         """
         Tests the proper initialization of the Label object, verifying that the name and color attributes are correct.
         """
-        self.assertEqual(self.label.name, "Test Label")
-        self.assertEqual(self.label.color, "blue")
-        self.mock_user.add_label.assert_called_with(self.label)
+        self.assertEqual(self.test_label.name, "Test Label")
+        self.assertEqual(self.test_label.color, "azul")
+                         
+    def test_label_save_to_db(self):
+        new_label = Label(user=self.test_user, name='New Label', color='verde', session=self.session)
+        new_label.save_to_db()
+
+        saved_label = self.session.query(LabelORM).filter_by(name='New Label').first()
+        self.assertIsNotNone(saved_label)
+        self.assertEqual(saved_label.color, 'verde')
+
 
     def test_delete(self) -> None:
-        """
-        Tests the deletion method of the Label object, verifying that the mock user's remove_label method is called.
-        """
-        self.label.delete()
-        self.mock_user.remove_label.assert_called_with(self.label)
+        label_id = self.test_label._id_label
+        self.test_label.delete()
+        
+        deleted_label = self.session.query(LabelORM).filter_by(id_label=label_id).first()
+        self.assertIsNone(deleted_label)
+
 
     def test_update_valid(self) -> None:
         """
         Tests valid update of the Label object by changing its name and color, and verifies that the new values are correct.
         """
-        self.label.update(name="New Label", color="red")
-        self.assertEqual(self.label.name, "New Label")
-        self.assertEqual(self.label.color, "red")
-        self.mock_user.update_label.assert_called_with(self.label)
+        self.test_label.update(name="New Label", color="red")
+        self.assertEqual(self.test_label.name, "New Label")
+        self.assertEqual(self.test_label.color, "red")
 
     def test_update_invalid(self) -> None:
         """
         Tests invalid update of the Label object by attempting to update a non-existent attribute and verifies that the correct exception is raised.
         """
         with self.assertRaises(ItemDontHaveThisAttribute):
-            self.label.update(task="New Task")
+            self.test_label.update(invalid_property="Invalid")
+
 
     def test_update_non_changeable_property(self) -> None:
         """
         Tests that updating a non-changeable property (user) raises the appropriate exception.
         """
         with self.assertRaises(NonChangeableProperty):
-            self.label.update(user=self.mock_user)
+            self.test_label.update(user=Mock())
 
-    def test_getters(self) -> None:
-        """
-        Tests the getters for name and color properties.
-        """
-        self.assertEqual(self.label.name, "Test Label")
-        self.assertEqual(self.label.color, "blue")
 
-    def test_setters(self) -> None:
-        """
-        Tests the setters for name and color properties.
-        """
-        self.label.name = "Updated Label"
-        self.label.color = "green"
-        self.assertEqual(self.label.name, "Updated Label")
-        self.assertEqual(self.label.color, "green")
+    def test_propertys(self):
+        self.assertEqual(self.test_label.user, self.test_user)
+        self.assertEqual(self.test_label.name, 'Test Label')
+        self.assertEqual(self.test_label.color, 'azul')
+        self.assertIsInstance(self.test_label.id_label, int)
+        self.assertEqual(self.test_label.id_label, self.test_label._id_label)
 
-    def test_update_with_invalid_data_type(self) -> None:
-        """
-        Tests updating the label with invalid data types for name and color properties.
-        """
-        with self.assertRaises(TypeError):
-            self.label.update(name=123)  # Assuming name should be a string
-
-        with self.assertRaises(TypeError):
-            self.label.update(color=456)  # Assuming color should be a string
             
 if __name__ == "__main__":
     unittest.main()
