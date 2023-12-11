6fd01583b6882b4e7e4dab0926a45bd6562bb32d
gabrielpastori <31520662+gabrielpastori@users.noreply.github.com>
2023-12-10 23:05:04 -0300

    add project manager test to __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,6 @@ from .test_modules.authentication_tests.test_instance_user import *
 from .test_modules.authentication_tests.test_login import *
 from .test_modules.authentication_tests.test_register import *
 from .test_modules.calendar_tests.calendar_display_tests import *
-# from .test_modules.calendar_tests.calendar_page_tests import *
 from .test_modules.calendar_tests.date_utilities_tests import *
 from .test_modules.calendar_tests.month_view_tests import *
 from .test_modules.calendar_tests.month_year_navigation_tests import *
@@ -15,25 +14,13 @@ from .test_modules.filter_tests.test_filter import *
 from .test_modules.history_tests.test_display_history import *
 from .test_modules.history_tests.test_history import *
 from .test_modules.homepage_tests.test_homepage import *
-# from .test_modules.items_tests.labels_tests.labels_logic_tests import *
 from .test_modules.items_tests.labels_tests.labels_page_tests import *
 from .test_modules.items_tests.project_tests.project_tests import *
 from .test_modules.items_tests.project_tests.project_create_page_tests import *
-# from .test_modules.items_tests.project_tests.project_manager_tests import *
-# from .test_modules.items_tests.project_tests.project_tests import *
-# from .test_modules.items_tests.project_tests.project_update_page_tests import *
+from .test_modules.items_tests.project_tests.project_manager_tests import *
 from .test_modules.items_tests.subtask_tests.subtask_create_page_tests import *
-# from .test_modules.items_tests.subtask_tests.subtask_manager_tests import *
-# from .test_modules.items_tests.subtask_tests.subtask_manager_tests import *
 from .test_modules.items_tests.subtask_tests.subtask_page_tests import *
-# from .test_modules.items_tests.subtask_tests.subtask_tests import *
-# from .test_modules.items_tests.subtask_tests.subtask_update_page_tests import *
 from .test_modules.items_tests.tasks_tests.task_create_page_tests import *
 from .test_modules.items_tests.tasks_tests.task_manager_tests import *
 from .test_modules.items_tests.tasks_tests.task_page_tests import *
-# from .test_modules.items_tests.tasks_tests.task_tests import *
-# from .test_modules.items_tests.item_factory_tests import *
 from .test_modules.load_tests.load import *
-# from .test_modules.notification_tests.notification_test import *
-# from .test_modules.user_tests.user_tests import *
-# from .test_modules.export import *
